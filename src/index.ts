cb908f4d8a358fc6d806b23d5205911fa6034327
yurikdotdev <172495341+yurikdotdev@users.noreply.github.com>
2024-12-13 21:12:21 +0700

    feat: add intial endpoint and scaffold project structure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
-import { Hono } from 'hono'
+import { OpenAPIHono } from '@hono/zod-openapi';
+import { apiReference } from '@scalar/hono-api-reference';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
 
-const app = new Hono()
+const app = new OpenAPIHono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(logger());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
-export default app
+app.get(
+  '/',
+  apiReference({
+    pageTitle: 'FeastFind API',
+    theme: 'elysiajs',
+    spec: {
+      url: '/openapi.json',
+    },
+  })
+);
+
+app.doc('/openapi.json', {
+  openapi: '3.1.0',
+  info: {
+    version: '0.0.1',
+    title: 'FeastFind API',
+    description: 'API for FeastFind.com',
+  },
+});
+
+export default app;
