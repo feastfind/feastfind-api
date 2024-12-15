a2d3e01023e9edc931a22d7caf3cffb15f0cf61f
M Haidar Hanif <git@mhaidarhanif.com>
2024-12-15 15:46:57 +0700

    feat: /users response schema

--- src/features/user/route.ts
+++ src/features/user/route.ts
@@ -2,20 +2,23 @@ import type { Context } from 'hono';
 import { OpenAPIHono, z } from '@hono/zod-openapi';
 import prisma from '../../lib/db';
 import { Prisma } from '@prisma/client';
+import { UserSchema } from '../../../prisma/generated/zod';
 
 const usersRoute = new OpenAPIHono();
 const API_TAGS = ['User'];
 
-// Get Users Route
 usersRoute.openapi(
   {
     method: 'get',
     path: '/',
-    summary: 'Users',
-    description: 'Get all users',
+    description: 'Get all users without pagination',
     tags: API_TAGS,
     responses: {
-      200: { description: 'Get all users' },
+      200: {
+        description: 'Get all users',
+        content: { 'application/json': { schema: UserSchema.array() } },
+      },
+      500: { description: 'Failed to get all users' },
     },
   },
   async (c) => {
