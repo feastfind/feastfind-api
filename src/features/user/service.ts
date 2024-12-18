af8b8c5ffc74c7c3f9a39b4271d74a3f14b19e0a
yurikdotdev <172495341+yurikdotdev@users.noreply.github.com>
2024-12-18 13:09:36 +0700

    feat(auth): implement middleware to authenticate user by token

--- src/features/user/service.ts
+++ src/features/user/service.ts
@@ -10,5 +10,18 @@ export const getUserByUsername = async (username: string): Promise<User | null> 
     where: {
       username,
     },
+    include: {
+      places: true,
+      menuItems: true,
+      menuItemReview: true,
+    }
+  })
+}
+
+export const getUserById = async (id: string): Promise<User | null> => {
+  return await prisma.user.findUnique({
+    where: {
+      id,
+    },
   })
 }
