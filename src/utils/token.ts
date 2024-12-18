9acfdfafc666d46dd11f889f6b8e216ab06b7199
yurikdotdev <172495341+yurikdotdev@users.noreply.github.com>
2024-12-18 13:07:51 +0700

    feat: add token and password utils

---
+++ src/utils/token.ts
@@ -0,0 +1,32 @@
+import { sign, verify } from 'hono/jwt';
+import { CONFIG } from '../config/config';
+
+import { JwtTokenExpired } from 'hono/utils/jwt/types';
+
+export const createToken = async (userId: string, expireInDays: number = 7) => {
+  try {
+    const exp = Math.floor(Date.now() / 1000) + expireInDays * 24 * 60 * 60;
+
+    const jwt = await sign({ userId, exp }, CONFIG.TOKEN_SECRET);
+
+    return jwt;
+  } catch (err) {
+    console.error('Error creating token:', err);
+    return null;
+  }
+};
+
+export const validateToken = async (token: string) => {
+  try {
+    const payload = await verify(token, CONFIG.TOKEN_SECRET);
+
+    if (payload instanceof JwtTokenExpired) {
+      console.error('Token expired');
+    }
+
+    return payload;
+  } catch (err) {
+    console.error('Error validating token:', err);
+    return null;
+  }
+};
