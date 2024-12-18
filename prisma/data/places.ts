ae7d47cff994fc77b1551bfeee08878463be89b1
Ananda Puja Wandra <anandapujawandra@gmail.com>
2024-12-18 21:12:31 +0700

    seed places data

---
+++ prisma/data/places.ts
@@ -0,0 +1,58 @@
+export const dataPlaces = [
+  {
+    slug: 'jambo-kupi',
+    name: 'Jambo Kupi',
+    description:
+      'Menu yang dijual tidak hanya mie aceh yang tersohor, namun juga ada berbagai macam makanan Khas Aceh lainnya seperti ayam tangkap, gulai bebek hingga timpan.',
+    priceMin: 50000,
+    priceMax: 75000,
+    city: 'Depok',
+    address:
+      'Jl. Margonda No.234 D, Kemiri Muka, Kecamatan Beji, Kota Depok, Jawa Barat 16424',
+    latitude: -6.381544959364254,
+    longitude: 106.83030138042663,
+    user: 'admin',
+  },
+  {
+    slug: 'sego-sambel-mak-yeye',
+    name: 'Sego Sambel Mak Yeye',
+    description:
+      'Mak Yeye adalah penyetan legendaris yang ada sejak tahun 1982. Kuliner ini ada di pinggir Jalan Jagir No 12 Wonokromo Wetan, tepatnya di sebelah utara Gedung Darmo Trade Centre (DTC).',
+    priceMin: 25000,
+    priceMax: 75000,
+    city: 'Surabaya',
+    address:
+      'Jl. Jagir Wonokromo Wetan No.10, Jagir, Kec. Wonokromo, Surabaya, Jawa Timur 60244',
+    latitude: -7.301111884047268,
+    longitude: 112.73917395662413,
+    user: 'admin',
+  },
+  {
+    slug: 'de-sampan',
+    name: 'De Sampan',
+    description:
+      'De Sampan Bbq Centre adalah tempat makan yang memiliki hidangan khas Makanan Asian, Makanan Indonesian dengan rating makanan 4 bintang.',
+    priceMin: 25000,
+    priceMax: 75000,
+    city: 'Batam',
+    address:
+      'Jl. Ruko Greenland Jl. Raja M Tahir No.2, Tlk. Tering, Kec. Batam Kota, Kota Batam, Kepulauan Riau 29461',
+    latitude: 1.125486491643992,
+    longitude: 104.04678114365636,
+    user: 'admin',
+  },
+  {
+    slug: 'the-manabu-bintan',
+    name: 'The Manabu Bintan',
+    description:
+      'Salah satunya The Manabu Bintan merupakan restoran yang menyajikan aneka pilihan menu barat dan nusantara.',
+    priceMin: 75000,
+    priceMax: 120000,
+    city: 'Tanjung Pinang',
+    address:
+      'Jl.Hang Tuah No.17 Sadaap, Tepi Laut, Kec. Tj. Pinang Kota, Kota Tanjung Pinang, Kepulauan Riau 29111',
+    latitude: 0.9261549301037288,
+    longitude: 104.43826874232784,
+    user: 'admin',
+  },
+];
