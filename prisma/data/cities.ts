f7a614eff210cf3def4cf3fbb0dc007c68d633fa
Ananda Puja Wandra <anandapujawandra@gmail.com>
2024-12-19 10:11:21 +0700

    complete seed places data

--- prisma/data/cities.ts
+++ prisma/data/cities.ts
@@ -29,4 +29,34 @@ export const dataCities = [
     latitude: 1.1030815,
     longitude: 104.0383696,
   },
+  {
+    slug: 'makassar',
+    name: 'Makassar',
+    latitude: -5.1342962,
+    longitude: 119.4124282,
+  },
+  {
+    slug: 'padang',
+    name: 'Padang',
+    latitude: -0.9247587,
+    longitude: 100.3632561,
+  },
+  {
+    slug: 'bali',
+    name: 'Bali',
+    latitude: -8.2271303,
+    longitude: 115.1919203,
+  },
+  {
+    slug: 'jayapura',
+    name: 'Jayapura',
+    latitude: -2.5387539,
+    longitude: 140.7037389,
+  },
+  {
+    slug: 'ambon',
+    name: 'Ambon',
+    latitude: 47.5553942,
+    longitude: -2.5545242,
+  },
 ];
--- prisma/data/places.ts
+++ prisma/data/places.ts
@@ -1,7 +1,7 @@
 export const dataPlaces = [
   {
-    slug: 'jambo-kupi',
-    name: 'Jambo Kupi',
+    slug: 'jambo-kupi-depok',
+    name: 'Jambo Kupi Depok',
     description:
       'Selain mie aceh yang tersohor, juga ada berbagai macam makanan khas Aceh lainnya seperti ayam tangkap, gulai bebek hingga timpan.',
     priceMin: 50000,
@@ -13,6 +13,35 @@ export const dataPlaces = [
     longitude: 106.83030138042663,
     username: 'admin',
   },
+  {
+    slug: 'mang-engking',
+    name: 'Gubug Makan Mang Engking',
+    description:
+      'Masakan seafood khas Indonesia seperti udang pedas & ikan goreng yang disajikan di geladak di atas air.',
+    priceMin: 50000,
+    priceMax: 100000,
+    citySlug: 'depok',
+    address:
+      'Kampus UI Jalan Danau Salam, RW.3, Srengseng Sawah, Kec. Jagakarsa, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 16424',
+    latitude: -6.349210107955499,
+    longitude: 106.83226355900068,
+    username: 'admin',
+  },
+  {
+    slug: 'pondok-ikan-bakar-kalimantan',
+    name: 'Pondok Ikan Bakar Kalimantan',
+    description:
+      'Makanan andalannya ikan patin bakar dalam bambu, bumbunya medok sekali sampai bisa dibawa pulang setelah ikannya habis. Selain itu banyak menu-menu lain.',
+    priceMin: 50000,
+    priceMax: 100000,
+    citySlug: 'depok',
+    address:
+      'Jl. Raya Tapos No.1, Tapos, Kec. Tapos, Kota Depok, Jawa Barat 16457',
+    latitude: -6.423537292986869,
+    longitude: 106.88954359762344,
+    username: 'admin',
+  },
+
   {
     slug: 'sego-sambel-mak-yeye',
     name: 'Sego Sambel Mak Yeye',
@@ -28,6 +57,35 @@ export const dataPlaces = [
     username: 'admin',
   },
   {
+    slug: 'nasi-jagung-khas-madura',
+    name: 'Nasi Jagung Khas Madura',
+    description:
+      'Nasi jagung khas madura di sini memiliki tekstur empuk dan tidak amis, sambalnya yang gurih dan pedas bikin nagih.',
+    priceMin: 10000,
+    priceMax: 25000,
+    citySlug: 'surabaya',
+    address:
+      'Ps. Pegirian, Jl. Nyamplungan No.236, Ampel, Kec. Semampir, Surabaya, Jawa Timur 60152',
+    latitude: -7.2268101369438735,
+    longitude: 112.74625268783674,
+    username: 'admin',
+  },
+  {
+    slug: 'rm-bebek-purnama',
+    name: 'RM Bebek Purnama',
+    description:
+      'Inayah menjelaskan, Bebek Purnama milik Umik Mardiah pertama kali dirintis pada tahun 1989. Lalu mulai populer sejak 1990-an.',
+    priceMin: 25000,
+    priceMax: 75000,
+    citySlug: 'surabaya',
+    address:
+      'Jl. Plumpang Semper, RT.8/RW.1, Tugu Sel., Kec. Koja, Jkt Utara, Daerah Khusus Ibukota Jakarta 14260',
+    latitude: -7.301111884047268,
+    longitude: 112.73917395662413,
+    username: 'admin',
+  },
+
+  {
     slug: 'de-sampan',
     name: 'De Sampan',
     description:
@@ -42,6 +100,35 @@ export const dataPlaces = [
     username: 'admin',
   },
   {
+    slug: 'altitude-rooftop-bar-and-lounge',
+    name: 'Altitude Rooftop Bar and Lounge',
+    description:
+      'Bar & lounge gaya hidup terbuka yang terinspirasi oleh teras apartemen New York, menyajikan tapas Asia dan koktail khas untuk dinikmati sambil menikmati pemandangan spektakuler.',
+    priceMin: 250000,
+    priceMax: 350000,
+    citySlug: 'batam',
+    address:
+      'Batam Marriott Hotel Harbour Bay (Level R, Jodoh River, Batu Ampar, Batam City, Riau Islands 29453',
+    latitude: 1.1543630565743248,
+    longitude: 103.99699853809875,
+    username: 'admin',
+  },
+  {
+    slug: 'sei-enam-seafood',
+    name: 'Sei Enam Seafood',
+    description:
+      'Restoran Sei Enam Batam, kami berkomitmen untuk menyediakan makanan laut segar berkualitas melalui proses pemilihan menyeluruh dan prosedur penanganan yang higienis.',
+    priceMin: 120000,
+    priceMax: 240000,
+    citySlug: 'batam',
+    address:
+      'Komplek Ruko Mahkota Raya, Jl. Raja H. Fisabilillah, RW.2, Teluk Tering, Batam Kota, Batam City, Riau Islands 29461',
+    latitude: 1.1305576588169108,
+    longitude: 104.03956458546018,
+    username: 'admin',
+  },
+
+  {
     slug: 'the-manabu-bintan',
     name: 'The Manabu Bintan',
     description:
@@ -53,6 +140,291 @@ export const dataPlaces = [
       'Jl.Hang Tuah No.17 Sadaap, Tepi Laut, Kec. Tj. Pinang Kota, Kota Tanjung Pinang, Kepulauan Riau 29111',
     latitude: 0.9261549301037288,
     longitude: 104.43826874232784,
+    username: 'admin',
+  },
+  {
+    slug: 'rm-mie-tarempa',
+    name: 'RM. Mie Tarempa',
+    description:
+      'Mie Tarempa merupakan salah satu kuliner yang cukup populer di Kota Batam. Mie Tarempa cukup berbeda, jika dibandingkan dengan beberapa mie yang dikenal masyarakat pada umumnya.',
+    priceMin: 25000,
+    priceMax: 50000,
+    citySlug: 'tanjung-pinang',
+    address:
+      'Jl. D.I panjaitan KM 7, Melayu Kota Piring, Tanjungpinang Timur, Melayu Kota Piring, Kec. Tanjungpinang Tim., Kota Tanjung Pinang, Kepulauan Riau 29123',
+    latitude: 0.9173812031050254,
+    longitude: 104.48820617805005,
+    username: 'admin',
+  },
+  {
+    slug: 'nasi-lemak-paradise',
+    name: 'Nasi Lemak Paradise',
+    description:
+      'Nasi Lemak Gesek. Nasi lemak merupakan makanan khas Melayu yang juga banyak ditemukan di Tanjung Pinang, dan kamu dapat mencoba nasi lemak paling legendaris.',
+    priceMin: 15000,
+    priceMax: 25000,
+    citySlug: 'tanjung-pinang',
+    address:
+      'Kemboja, Kec. Tanjungpinang Bar., Kota Tanjung Pinang, Kepulauan Riau',
+    latitude: 0.9262377575339156,
+    longitude: 104.45024654868995,
+    username: 'admin',
+  },
+
+  {
+    slug: 'rumah-makan-khas-sunda-cibiuk',
+    name: 'Rumah Makan Khas Sunda Cibiuk',
+    description:
+      'Tempat makan yang menghidangkan aneka masakan khas Sunda dengan resep tradisional dan aneka minuman.',
+    priceMin: 50000,
+    priceMax: 75000,
+    citySlug: 'bandung',
+    address:
+      'Jl. Soekarno-Hatta No.508, Batununggal, Kec. Bandung Kidul, Kota Bandung, Jawa Barat 40266',
+    latitude: -6.946593673881793,
+    longitude: 107.63808566004869,
+    username: 'admin',
+  },
+  {
+    slug: 'warung-nasi-ibu-mas',
+    name: 'Warung Nasi Ibu Mas',
+    description:
+      'Hidangan tradisional Sunda disajikan bersama sambal pedas di tempat simpel ini.',
+    priceMin: 25000,
+    priceMax: 50000,
+    citySlug: 'bandung',
+    address:
+      'Jl. Balonggede No.67 / 48 / 38 / 93, Balonggede, Kec. Regol, Kota Bandung, Jawa Barat 40251',
+    latitude: -6.9256894131966,
+    longitude: 107.60783572831207,
+    username: 'admin',
+  },
+  {
+    slug: 'rumah-makan-sunda-tabulla-rasa',
+    name: 'Rumah Makan Sunda Tabulla Rasa',
+    description:
+      'Tabulla Rasa Bandung adalah rumah makan yang menyajikan aneka masakan Sunda. Tempatnya yang nyaman dan didesain dengan menggunakan ornamen bambu.',
+    priceMin: 50000,
+    priceMax: 75000,
+    citySlug: 'bandung',
+    address:
+      'Jl. Soekarno-Hatta No.88, Wr. Muncang, Kec. Bandung Kulon, Kota Bandung, Jawa Barat 40211',
+    latitude: -6.946593673881793,
+    longitude: 107.63808566004869,
+    username: 'admin',
+  },
+
+  {
+    slug: 'pallubasa-onta',
+    name: 'Pallubasa Onta',
+    description:
+      'Pallu basa adalah makanan khas Makassar. Kuliner ini mirip dengan Rawon di Jawa, namun tidak pakai bumbu hitam. Ada bumbu khas tambahan dengan serbuk parutan.',
+    priceMin: 25000,
+    priceMax: 50000,
+    citySlug: 'makassar',
+    address:
+      'Jl. Onta Lama No.103, Mandala, Kec. Mamajang, Kota Makassar, Sulawesi Selatan 90132',
+    latitude: -5.157584447154766,
+    longitude: 119.42341364180383,
+    username: 'admin',
+  },
+  {
+    slug: 'coto-nusantara',
+    name: 'Coto Nusantara',
+    description:
+      'Coto Nusantara berlokasi di Jalan Nusantara Baru No.142, Kecamatan Wajo, Kota Makassar. Lokasinya terbilang cukup strategis dan dekat dari pusat.',
+    priceMin: 25000,
+    priceMax: 50000,
+    citySlug: 'makassar',
+    address:
+      'Jl. Nusantara Baru No.86, Pattunuang, Kec. Wajo, Kota Makassar, Sulawesi Selatan 90173',
+    latitude: -5.157584447154766,
+    longitude: 119.42341364180383,
+    username: 'admin',
+  },
+  {
+    slug: 'rumah-makan-kota-daeng',
+    name: 'Rumah Makan Kota Daeng',
+    description:
+      'RM Kota Daeng adalah tempat yang tepat untuk menikmati kelezatan masakan seafood khas bugis Makassar. Beberapa hidangan favorit kami adalah ikan goreng.',
+    priceMin: 40000,
+    priceMax: 100000,
+    citySlug: 'makassar',
+    address:
+      'Jl. Perintis Kemerdekaan KM 18. Ruko Bps Nomor 9, Pai, Kec. Biringkanaya, Kota Makassar, Sulawesi Selatan 90242',
+    latitude: -5.076114147995184,
+    longitude: 119.52405160503356,
+    username: 'admin',
+  },
+
+  {
+    slug: 'katupek-pitalah-purus',
+    name: 'Katupek Pitalah Purus',
+    description:
+      'Katupek Gulai. Katupek Dengan Gulai Khas Pitalah ( Nangka, Rebung, Buncis, Sayur Kol) Yang Mana Sayurnya Di Gabung Kesemuanya.',
+    priceMin: 10000,
+    priceMax: 25000,
+    citySlug: 'padang',
+    address:
+      'Jl. Purus III No.19, Purus, Kec. Padang Bar, Kota Padang, Sumatera Barat 25115',
+    latitude: -0.9381147431466422,
+    longitude: 100.35336860503361,
+    username: 'admin',
+  },
+  {
+    slug: 'rumah-makan-lamun-ombak',
+    name: 'Rumah Makan Lamun Ombak',
+    description:
+      'RM Lamun Ombak merupakan rumah makan masakan otentik Padang. Menunya banyak sekali yang dihidangkan di meja.',
+    priceMin: 25000,
+    priceMax: 50000,
+    citySlug: 'padang',
+    address:
+      'Jl. Khatib Sulaiman No.99, Ulak Karang Sel., Kec. Padang Utara, Kota Padang, Sumatera Barat',
+    latitude: -0.9084116964621611,
+    longitude: 100.35380185344309,
+    username: 'admin',
+  },
+  {
+    slug: 'sate-danguang-danguang-simpang-kinol',
+    name: 'Sate Danguang Danguang Simpang Kinol',
+    description:
+      'Terletak di Jalan Simpang Kinol, Padang, Sate Danguang-Danguang Simpang Kinol Pondok telah menjadi salah satu tempat berburu sate Padang.',
+    priceMin: 25000,
+    priceMax: 50000,
+    citySlug: 'padang',
+    address:
+      'Jl. Tepi Pasang No.67 Kel, Kp. Pd., Kec. Padang Bar., Kota Padang, Sumatera Barat 25134',
+    latitude: -0.9578664536229737,
+    longitude: 100.36217908175512,
+    username: 'admin',
+  },
+
+  {
+    slug: 'rumah-kopi-sibu-sibu',
+    name: 'Rumah Kopi Sibu Sibu',
+    description:
+      'Lokasinya persis pinggir jalan, kanan-kirinya jualan persis sama dg Sibu-Sibu. Tempat ini ada live musicnya. Wajib order kalo kesini, sukun goreng & pisang.',
+    priceMin: 25000,
+    priceMax: 50000,
+    citySlug: 'ambon',
+    address: 'Said Perintah, Kel Ahusen, Kec. Sirimau, Kota Ambon, Maluku',
+    latitude: -0.9381147431466422,
+    longitude: 100.35336860503361,
+    username: 'admin',
+  },
+  {
+    slug: 'sari-gurih-restaurant',
+    name: 'Sari Gurih Restaurant',
+    description:
+      'Rumah makan ini salah satu pilihan untuk makan seafood di kota Ambon. Masakannya cukup lezat dan pelayanannya cepat. Selain makan ada tersedia live music.',
+    priceMin: 25000,
+    priceMax: 50000,
+    citySlug: 'ambon',
+    address:
+      'Jl. Anthony Rebok, Kel Honipopu, Kec. Sirimau, Kota Ambon, Maluku 97127',
+    latitude: -3.6967358165252437,
+    longitude: 128.18161006218173,
+    username: 'admin',
+  },
+  {
+    slug: 'dapor-kole-kole',
+    name: 'Dapor Kole Kole',
+    description:
+      'Menyajikan Kuliner Ambon dan Nusantara. Best Tuna dan Cakalang lokal bisa dapatkan di sini, Bistik Tuna, Mie Kuah cakalang, Menu Sapi dan ayam juga tersedia.',
+    priceMin: 50000,
+    priceMax: 75000,
+    citySlug: 'ambon',
+    address:
+      'Jl. Dana Kopra No.16, Uritetu, Kec. Sirimau, Kota Ambon, Maluku 97124',
+    latitude: -3.6950148160993943,
+    longitude: 128.1838222915418,
+    username: 'admin',
+  },
+
+  {
+    slug: 'rumah-laut',
+    name: 'Rumah Laut',
+    description:
+      'Restoran ini eksotis karena ada di tepi laut Jayapura sehingga bisa melihat keindahan kota di waktu malam. Suasana baik dan ada iringan musik dan penyanyi lokal.',
+    priceMin: 35000,
+    priceMax: 85000,
+    citySlug: 'jayapura',
+    address:
+      'Jl. Koti, Numbai, Distrik Jayapura Selatan, Kota Jayapura, Papua 99222',
+    latitude: -2.544127630450188,
+    longitude: 140.7091864756735,
+    username: 'admin',
+  },
+  {
+    slug: 'rumah-makan-sendok-garpu',
+    name: 'Rumah Makan Sendok Garpu',
+    description:
+      'Lokasi cukup strategis di kawasan kotaraja abepura, menu ikan mujair gorengnya lumayan enak dan bisa mencoba papedanya, suasana nyaman untuk kumpul bersama.',
+    priceMin: 16000,
+    priceMax: 40000,
+    citySlug: 'jayapura',
+    address:
+      'Jl. Raya Abepura-Kotaraja Kota Baru, Wai Mhorock, Kec. Abepura, Kota Jayapura, Papua 99225',
+    latitude: -2.605483168511364,
+    longitude: 140.67023546270573,
+    username: 'admin',
+  },
+  {
+    slug: 'yougwa-restaurant',
+    name: 'Yougwa Restaurant',
+    description:
+      'Bangunannya terapung di tepi Danau Sentani, asyik banget untuk menikmati pemandangan ditemani angin sepoi-sepoi. Resto ini punya spesialisasi ikan dan seafood.',
+    priceMin: 50000,
+    priceMax: 100000,
+    citySlug: 'jayapura',
+    address:
+      'Jl. Raya Abepura - Sentani, Asei Kecil, Kec. Sentani Tim., Kabupaten Jayapura, Papua 99351',
+    latitude: -2.588458372730069,
+    longitude: 140.6037499008045,
+    username: 'admin',
+  },
+
+  {
+    slug: 'ayam-tempong-ibu-sri',
+    name: 'Ayam Tempong Ibu Sri Kuta Legian',
+    description:
+      'Tempong Bu Sri menyajikan beberapa menu varian nasi tempong. Antara lain, nasi tempong ayam, bebek, cumi, ikan, udang tahu tempe, dan masih banyak lagi. Soal lokasi, ada empat Warung Ayam Tempong Ibu Sri.',
+    priceMin: 10000,
+    priceMax: 25000,
+    citySlug: 'bali',
+    address:
+      'Jl. Mataram No.52-48, Kuta, Kec. Kuta, Kabupaten Badung, Bali 80361',
+    latitude: -8.719834858988087,
+    longitude: 115.18106301244374,
+    username: 'admin',
+  },
+  {
+    slug: 'es-waneng',
+    name: 'Es Waneng',
+    description:
+      'Es Waneng di Tabanan bisa ni dinobatin jadi es legend Rasanya manis dan berasa bgt santan kelapanya.',
+    priceMin: 10000,
+    priceMax: 25000,
+    citySlug: 'bali',
+    address:
+      'Jl. Mawar No.25, Delod Peken, Kec. Tabanan, Kabupaten Tabanan, Bali 82121',
+    latitude: -8.545823491149218,
+    longitude: 115.12520567249243,
+    username: 'admin',
+  },
+  {
+    slug: 'teba-cafe-jimbaran-official',
+    name: 'Teba Cafe Jimbaran Official',
+    description:
+      'Penataan dan penyajiannya bagus. Tersedia menu seperti Sup ikan, Nasi goreng, Mie goreng, Cumi asam manis, Udang goreng tepung, Cumi goreng tepung dan lainnya.',
+    priceMin: 100000,
+    priceMax: 200000,
+    citySlug: 'bali',
+    address:
+      'Jl. Pantai Muaya four season hotel, Jimbaran, Badung Regency, Bali 80361',
+    latitude: -8.78062867370843,
+    longitude: 115.16548379339436,
     username: 'admin',
   },
 ];
