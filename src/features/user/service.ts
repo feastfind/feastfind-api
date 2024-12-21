3bfe6b078ac88da6c60ff426bdf82a1303d1e8fc
yurikdotdev <172495341+yurikdotdev@users.noreply.github.com>
2024-12-21 19:19:22 +0700

    refactor: simplify get by param logic

--- src/features/user/service.ts
+++ src/features/user/service.ts
@@ -1,44 +1,21 @@
 import { User } from '../../../prisma/generated/zod';
 import prisma from '../../lib/db';
+import { isValidCUID, isValidEmail } from '../../utils/regex';
 
 export const getAllUsers = async (): Promise<User[]> => {
   return await prisma.user.findMany();
 };
 
 export const getUserByParam = async (param: string): Promise<User | null> => {
-  const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(param);
-  const isCUID = /^[a-z0-9]{25}$/.test(param);
+  const isEmail = isValidEmail(param);
+  const isCUID = isValidCUID(param);
 
-  if (isEmail) {
-    return await prisma.user.findUnique({
-      where: {
-        email: param,
-      },
-      include: {
-        places: true,
-        menuItems: true,
-        menuItemReview: true,
-      },
-    });
-  }
-
-  if (isCUID) {
-    return await prisma.user.findUnique({
-      where: {
-        id: param,
-      },
-      include: {
-        places: true,
-        menuItems: true,
-        menuItemReview: true,
-      },
-    });
-  }
-
-  return await prisma.user.findFirst({
-    where: {
-      OR: [{ username: param }, { email: param }, { id: param }],
-    },
+  return await prisma.user.findUnique({
+    where: isEmail
+      ? { email: param }
+      : isCUID
+        ? { id: param }
+        : { username: param },
     include: {
       places: true,
       menuItems: true,
