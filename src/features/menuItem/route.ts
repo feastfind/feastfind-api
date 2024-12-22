93939850600e66966ea3316b31c3864da79b69b6
yurikdotdev <172495341+yurikdotdev@users.noreply.github.com>
2024-12-22 10:28:53 +0700

    feat(route): add GET menuItemReviews by menuItem param

--- src/features/menuItem/route.ts
+++ src/features/menuItem/route.ts
@@ -1,7 +1,11 @@
 import { OpenAPIHono, z } from '@hono/zod-openapi';
 import { MenuItemSchema } from '../../../prisma/generated/zod';
 import { handleErrorResponse } from '../../utils/handleError';
-import { getMenuItems, getMenuItemByParam} from './service'
+import {
+  getMenuItemByParam,
+  getMenuItemReviewsByMenuItemParam,
+  getMenuItems,
+} from './service';
 
 const menuItemsRoute = new OpenAPIHono();
 const API_TAGS = ['MenuItem'];
@@ -77,6 +81,62 @@ menuItemsRoute.openapi(
       return handleErrorResponse(
         c,
         `Failed to retrieve menu item: ${error} `,
+        500
+      );
+    }
+  }
+);
+
+menuItemsRoute.openapi(
+  {
+    method: 'get',
+    path: '/{param}/reviews',
+    description: 'Get menu item reviews by menu param.',
+    tags: ['MenuItemReview'],
+    request: {
+      params: z.object({
+        param: z
+          .string()
+          .max(255)
+          .openapi({ description: 'param: menu slug | menu id' }),
+      }),
+    },
+    responses: {
+      200: {
+        description: 'Menu item reviews retrieved successfully',
+        content: { 'application/json': { schema: MenuItemSchema } },
+      },
+      400: {
+        description: 'Invalid menu param',
+      },
+      404: {
+        description: 'Menu item reviews not found',
+      },
+      500: {
+        description: 'Failed to retrieve menu item reviews',
+      },
+    },
+  },
+  async (c) => {
+    try {
+      const { param } = c.req.valid('param');
+
+      const menuItem = await getMenuItemByParam(param);
+
+      if (!menuItem) return handleErrorResponse(c, 'Menu item not found', 404);
+
+      const menuItemReviews = await getMenuItemReviewsByMenuItemParam(
+        menuItem.id
+      );
+
+      if (!menuItemReviews)
+        return handleErrorResponse(c, 'Menu item reviews not found', 404);
+
+      return c.json(menuItemReviews, 200);
+    } catch (error) {
+      return handleErrorResponse(
+        c,
+        `Failed to retrieve menu item reviews: ${error} `,
         500
       );
     }
