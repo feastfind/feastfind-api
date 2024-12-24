1f27bc62041245c24cde9daf4c6210935cd17020
Ananda Puja Wandra <anandapujawandra@gmail.com>
2024-12-24 23:37:22 +0700

    seed : add user data in seeding

--- prisma/data/users.ts
+++ prisma/data/users.ts
@@ -5,4 +5,34 @@ export const dataUsers = [
     email: 'admin@feastfind.com',
     password: 'secret_admin_sentence',
   },
+  {
+    name: 'Puja',
+    username: 'puja',
+    email: 'puja@feastfind.com',
+    password: 'secret_admin_sentence',
+  },
+  {
+    name: 'Yurik',
+    username: 'yurik',
+    email: 'yurik@feastfind.com',
+    password: 'secret_admin_sentence',
+  },
+  {
+    name: 'Obby',
+    username: 'obby',
+    email: 'obby@feastfind.com',
+    password: 'secret_admin_sentence',
+  },
+  {
+    name: 'Nanda',
+    username: 'nanda',
+    email: 'nanda@feastfind.com',
+    password: 'secret_admin_sentence',
+  },
+  {
+    name: 'Haidar',
+    username: 'haidar',
+    email: 'haidar@feastfind.com',
+    password: 'secret_admin_sentence',
+  },
 ];
