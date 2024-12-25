cacd4abba32387287ebbe23da9ea2e42f055b345
Ananda Puja Wandra <anandapujawandra@gmail.com>
2024-12-25 20:29:50 +0700

    fix bug : edit prisma schema and complete seeder

---
+++ prisma/data/menuItemsReview.ts
@@ -0,0 +1,260 @@
+export const dataMenuItemsReview = [
+  {
+    menuItemSlug: 'mie-aceh-super-spesial',
+    username: 'puja',
+    rating: 5,
+    comment:
+      'Gak pernah gagal deh setiap kali makan Mie Aceh! Rasa pedasnya itu tuh bener-bener nendang banget, tapi tetap ada keseimbangan antara gurih dan manisnya. Apalagi kalau pakai tambahan daging kambing atau udang, makin mantap!',
+  },
+  {
+    menuItemSlug: 'martabak-daging',
+    username: 'puja',
+    rating: 4,
+    comment:
+      'Setiap suapan terasa kaya, apalagi dengan tambahan telur dan bawang bombay yang memberi sensasi segar dan gurih.',
+  },
+  {
+    menuItemSlug: 'nasi-goreng-bistik',
+    username: 'puja',
+    rating: 5,
+    comment:
+      'Nasi gorengnya wangi banget, bumbunya pas, dan rasanya gurih dengan sedikit sentuhan manis. Tapi yang bikin spesial adalah potongan bistik daging sapi yang juicy dan empuk! Dagingnya nggak terlalu tebal, tapi cukup tebal untuk memberikan rasa yang kaya.',
+  },
+  {
+    menuItemSlug: 'udang-saus-asam-manis',
+    username: 'puja',
+    rating: 4,
+    comment:
+      'Saus asam manisnya pas banget, bikin nagih! Perpaduan rasa asam, manis, dan gurihnya bikin makanan ini makin lezat.',
+  },
+  {
+    menuItemSlug: 'gurame-bakar-madu',
+    username: 'puja',
+    rating: 5,
+    comment:
+      'Gurame bakar madu di restoran ini selalu jadi favoritku. Rasanya autentik dan bikin kenyang.',
+  },
+  {
+    menuItemSlug: 'kepiting-lemuri-saos-padang',
+    username: 'puja',
+    rating: 5,
+    comment:
+      'Kepiting lemuri saus padangnya luar biasa! Dagingnya tebal dan manis, sausnya pedas nampol, perpaduan yang sempurna!',
+  },
+  {
+    menuItemSlug: 'ikan-bakar-khas-kalimantan',
+    username: 'puja',
+    rating: 3,
+    comment:
+      'Bumbunya agak terlalu asin, mungkin bisa dikurangi sedikit garamnya.',
+  },
+  {
+    menuItemSlug: 'sego-sambel',
+    username: 'puja',
+    rating: 4,
+    comment:
+      'Yang paling aku suka dari Sego Sambel adalah cara penyajiannya yang sederhana tapi menggugah selera. Selain sambel, biasanya ada tambahan lauk seperti ikan asin, tempe, atau ayam goreng yang makin nambah kaya rasa.',
+  },
+  {
+    menuItemSlug: 'nasi-jagung',
+    username: 'puja',
+    rating: 4,
+    comment:
+      'Nasi sambel ini jadi comfort food yang sempurna buat yang pengen makan yang praktis, tapi tetap penuh rasa.',
+  },
+  {
+    menuItemSlug: 'bebek-goreng',
+    username: 'puja',
+    rating: 5,
+    comment:
+      'Bebek Goreng ini benar-benar juara! Dari luar, kulitnya yang digoreng garing banget, bikin setiap gigitan terasa crispy dan gurih. ',
+  },
+  {
+    menuItemSlug: 'cah-kangkung',
+    username: 'yurik',
+    rating: 3,
+    comment:
+      'Bumbunya kurang berasa, mungkin bisa ditambah sedikit bawang putih atau cabai.',
+  },
+  {
+    menuItemSlug: 'kepiting-saus-padang',
+    username: 'yurik',
+    rating: 5,
+    comment:
+      'Daging kepitingnya sangat segar dan manis, teksturnya lembut. Saus padangnya kaya akan rempah, pedasnya pas, dan bikin nafsu makan bertambah.',
+  },
+  {
+    menuItemSlug: 'kerang-rebus',
+    username: 'yurik',
+    rating: 5,
+    comment:
+      'Bumbu rebusannya meresap sempurna ke dalam daging kerang, rasanya pas di lidah. Tambahan sambalnya juga bikin semakin mantap.',
+  },
+  {
+    menuItemSlug: 'kebab',
+    username: 'yurik',
+    rating: 4,
+    comment:
+      'Porsi dagingnya melimpah, dan rasanya tuh bener-bener menggugah selera. Setiap gigitan bikin ketagihan',
+  },
+  {
+    menuItemSlug: 'pizza',
+    username: 'yurik',
+    rating: 4,
+    comment:
+      'Dari crust yang crispy di bagian pinggir sampai toppingnya yang melimpah, setiap suapan terasa sempurna. Keju yang meleleh di atasnya itu, wah, gak ada yang bisa ngalahin pizzanya',
+  },
+  {
+    menuItemSlug: 'steak',
+    username: 'yurik',
+    rating: 4,
+    comment:
+      'Steak-nya luar biasa! Dagingnya empuk, bumbunya meresap, dan tingkat kematangannya pas banget.',
+  },
+  {
+    menuItemSlug: 'mie-goreng',
+    username: 'yurik',
+    rating: 3,
+    comment:
+      'Mie gorengnya terlalu berminyak dan rasanya terlalu asin. Mienya juga agak lembek, kurang kenyal.',
+  },
+  {
+    menuItemSlug: 'kulit-ikan-telur-asin',
+    username: 'yurik',
+    rating: 3,
+    comment:
+      'Porsinya cukup banyak, cocok untuk dibagi bersama teman atau keluarga.',
+  },
+  {
+    menuItemSlug: 'ikan-fillet',
+    username: 'yurik',
+    rating: 4,
+    comment:
+      'Ikan filletnya sangat lembut dan juicy! Bumbunya meresap sempurna, bikin ketagihan.',
+  },
+  {
+    menuItemSlug: 'cafe-latte',
+    username: 'yurik',
+    rating: 4,
+    comment:
+      'Cafe Latte ini cocok banget buat dinikmati kapan saja, baik untuk menemani pagi hari yang sibuk atau sekadar relaksasi di sore hari. ',
+  },
+  {
+    menuItemSlug: 'brown-sugar-cinnamon-danish',
+    username: 'nanda',
+    rating: 4,
+    comment:
+      'Brown sugar cinnamon danish-nya lembut banget! Perpaduan rasa manis dan harum kayu manisnya pas sekali.',
+  },
+  {
+    menuItemSlug: 'ta-mie-tarempa',
+    username: 'nanda',
+    rating: 5,
+    comment:
+      'Ta Mie Tarempa ini benar-benar bikin lidah bergoyang! Mie-nya kenyal dan teksturnya pas banget, berpadu sempurna dengan bumbu rempah yang kaya dan sedikit pedas.',
+  },
+  {
+    menuItemSlug: 'nasi-lemak',
+    username: 'nanda',
+    rating: 5,
+    comment:
+      'Yang paling juara adalah sambal yang pedasnya pas, memberikan sentuhan rasa manis dan sedikit asam yang bikin nasi lemak semakin nikmat. ',
+  },
+  {
+    menuItemSlug: 'gurame-crispy',
+    username: 'nanda',
+    rating: 5,
+    comment:
+      'Gurame crispy di restoran ini selalu jadi favoritku. Rasanya yang renyah dan gurih bikin ketagihan.',
+  },
+  {
+    menuItemSlug: 'paket-nasi-liwet-ayam-goreng',
+    username: 'nanda',
+    rating: 4,
+    comment:
+      'Nasi liwetnya wangi banget, santannya pas. Ayam gorengnya krispi di luar, lembut di dalam. Perpaduan yang sempurna!',
+  },
+  {
+    menuItemSlug: 'gurame-cobek',
+    username: 'nanda',
+    rating: 5,
+    comment:
+      'Daging gurame yang lembut dan empuk dipadu dengan sambal cobek yang pedas dan segar, rasanya tuh bener-bener pas di lidah! Ikan gurame yang digoreng garing di luar, tapi tetap juicy di dalam, membuat setiap gigitan semakin nikmat.',
+  },
+  {
+    menuItemSlug: 'gurame-pesmol',
+    username: 'nanda',
+    rating: 4,
+    comment:
+      'Setiap suapan ikan gurame yang dibalur dengan kuah pesmol itu, bener-bener memberikan kenikmatan yang tiada habisnya!',
+  },
+  {
+    menuItemSlug: 'pallubasa',
+    username: 'obby',
+    rating: 5,
+    comment:
+      'Yang paling aku suka dari Pallubasa adalah kuahnya yang kental dan kaya rasa, dengan sedikit sentuhan kecap manis, santan, dan rempah-rempah seperti bawang putih, jahe, dan serai.',
+  },
+  {
+    menuItemSlug: 'coto-makassar',
+    username: 'obby',
+    rating: 5,
+    comment:
+      'Warung ini punya Coto Makassar terenak yang pernah saya coba. Rasanya asli Makassar banget!',
+  },
+  {
+    menuItemSlug: 'bebek-rica-rica',
+    username: 'obby',
+    rating: 5,
+    comment:
+      'Bebek rica-ricanya mantap banget! Dagingnya empuk, bumbunya meresap, dan pedasnya pas banget.',
+  },
+  {
+    menuItemSlug: 'katupek-pitalah',
+    username: 'obby',
+    rating: 4,
+    comment:
+      'Katupek Pitalah ini bener-bener hidangan yang unik dan nggak pernah mengecewakan!',
+  },
+  {
+    menuItemSlug: 'rendang',
+    username: 'obby',
+    rating: 5,
+    comment: 'Rendang ini benar-benar juara!',
+  },
+  {
+    menuItemSlug: 'sarmento',
+    username: 'haidar',
+    rating: 3,
+    comment:
+      'Sarmento-nya agak terlalu asin, mungkin bisa dikurangi sedikit garamnya.',
+  },
+  {
+    menuItemSlug: 'sate-tuna',
+    username: 'haidar',
+    rating: 5,
+    comment:
+      'Daging tuna yang dipilih segar, dipotong tebal, dan dibumbui dengan sempurna, memberikan rasa yang kaya dan gurih. Satenya mantap',
+  },
+  {
+    menuItemSlug: 'paket-bawal-bakar-goreng',
+    username: 'haidar',
+    rating: 5,
+    comment:
+      'Daging bawalnya yang mudah lepas dari tulang dan rasa gurih yang meresap sempurna di setiap suapan, bikin kamu terus ingin nambah lagi! ',
+  },
+  {
+    menuItemSlug: 'sapo-tahu-seafood',
+    username: 'haidar',
+    rating: 4,
+    comment:
+      'Restoran ini punya sapo tahu terenak yang pernah saya coba. Rasanya autentik banget!',
+  },
+  {
+    menuItemSlug: 'nasi-tempong-ikan-lele-goreng-spesial',
+    username: 'haidar',
+    rating: 5,
+    comment:
+      '"Nasi tempongnya mantap banget! Pedasnya bikin nagih, ikan lelenya garing di luar lembut di dalam. Perpaduan yang pas!',
+  },
+];
