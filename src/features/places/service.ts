657ec5ea14cf248d66e50d086e686483e5a5f4a7
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-07 21:07:40 +0700

    fix: get palce include menu item images

--- src/features/places/service.ts
+++ src/features/places/service.ts
@@ -8,7 +8,10 @@ export const getPlaces = async (): Promise<{
   places: Place[];
   count: number;
 }> => {
-  const places = await prisma.place.findMany();
+  const places = await prisma.place.findMany({
+    include: { menuItems: { select: { images: true } } },
+  });
+
   const count = await prisma.place.count();
 
   return { places, count };
@@ -20,7 +23,7 @@ export const getPlaceByParam = async (param: string): Promise<Place | null> => {
       OR: [{ id: param }, { slug: param }],
     },
     include: {
-      menuItems: true,
+      menuItems: { select: { images: true } },
     },
   });
 };
