8acd2faf0bf00aa3a433327055303966898c3591
Ananda Puja Wandra <anandapujawandra@gmail.com>
2025-01-16 11:22:25 +0700

    fix : menuItemReviews schema

--- src/features/review/schema.ts
+++ src/features/review/schema.ts
@@ -9,12 +9,12 @@ import { MenuItemImage } from '../menuItemImage/schema';
 
 const menuItemWithImageSchema = MenuItemSchema.extend({
   images: MenuItemImage.array(),
+  place: PlaceSchema,
 });
 
 export const MenuItemsReviews = MenuItemReviewSchema.extend({
   menuItem: menuItemWithImageSchema,
   user: UserSchema,
-  place: PlaceSchema,
 });
 
 export const MenuItemsReviewsArray = MenuItemsReviews.array();
