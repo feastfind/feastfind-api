4fc2db4d043d0207aed8f6134126a1c21c367714
Ananda Puja Wandra <anandapujawandra@gmail.com>
2025-01-20 07:42:25 +0700

    feature : review pagination and popularity

--- src/features/review/route.ts
+++ src/features/review/route.ts
@@ -1,6 +1,6 @@
 import { API_TAGS } from '@/config';
 import { handleErrorResponse } from '@/utils/handleError';
-import { OpenAPIHono } from '@hono/zod-openapi';
+import { OpenAPIHono, z } from '@hono/zod-openapi';
 
 import * as reviewService from '@review/service';
 import { MenuItemsReviewsArray } from './schema';
@@ -14,6 +14,12 @@ reviewRoute.openapi(
     summary: 'Get all reviews',
     description: 'Get a list of reviews.',
     tags: API_TAGS.REVIEW,
+    request: {
+      query: z.object({
+        popularity: z.string().optional(),
+        page: z.string().optional(),
+      }),
+    },
     responses: {
       200: {
         description: 'Reviews retrieved successfully',
@@ -31,8 +37,20 @@ reviewRoute.openapi(
   },
   async (c) => {
     try {
-      const reviews = await reviewService.getReviews();
+      const { popularity, page } = c.req.valid('query');
 
+      if (popularity === 'highest') {
+        const reviews = await reviewService.getHighestReviews(page);
+
+        return c.json(reviews, 200);
+      }
+
+      if (popularity === 'lowest') {
+        const reviews = await reviewService.getLowestReviews(page);
+        return c.json(reviews, 200);
+      }
+
+      const reviews = await reviewService.getReviews(page);
       return c.json(reviews, 200);
     } catch (error) {
       return handleErrorResponse(
--- src/features/review/service.ts
+++ src/features/review/service.ts
@@ -1,12 +1,49 @@
 import prisma from '@/lib/db';
 
-export const getReviews = async () => {
+const takeItem = 10;
+
+export const getReviews = async (page?: string | undefined) => {
+  const skipItem = page ? Number(page) * takeItem - takeItem : 0;
   const reviews = await prisma.menuItemReview.findMany({
     include: {
       menuItem: { include: { images: true, place: true } },
       user: true,
     },
+    skip: skipItem,
+    take: takeItem,
   });
 
+  return reviews;
+};
+
+const findManyMenuItemReview = async (
+  takeItem: number,
+  skipItem: number,
+  order: 'desc' | 'asc'
+) => {
+  return await prisma.menuItemReview.findMany({
+    include: {
+      menuItem: { include: { images: true, place: true } },
+      user: true,
+    },
+    skip: skipItem,
+    take: takeItem,
+    orderBy: {
+      rating: order,
+    },
+  });
+};
+
+export const getHighestReviews = async (page?: string | undefined) => {
+  const skipItem = page ? Number(page) * takeItem - takeItem : 0;
+
+  const reviews = findManyMenuItemReview(takeItem, skipItem, 'desc');
+  return reviews;
+};
+
+export const getLowestReviews = async (page?: string | undefined) => {
+  const skipItem = page ? Number(page) * takeItem - takeItem : 0;
+
+  const reviews = findManyMenuItemReview(takeItem, skipItem, 'asc');
   return reviews;
 };
